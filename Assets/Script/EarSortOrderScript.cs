00a09d40bd0bb74104de94dd2218bf802ac889b6
Kurt Kaiser <kurtbkaiser@gmail.com>
2021-05-24 22:28:35 -0400

    changes
    d

--- Assets/Script/EarSortOrderScript.cs
+++ Assets/Script/EarSortOrderScript.cs
@@ -6,17 +6,5 @@ public class EarSortOrderScript : MonoBehaviour
 {
     bool earsOnTop = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if ((Input.GetKey(KeyCode.RightArrow)
-            || Input.GetKey(KeyCode.LeftArrow))
-            && earsOnTop == false) {
-            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Head";
-            earsOnTop = true;
-        } else if (earsOnTop == true)
-        {
-            gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Ears";
-        }
-    }
+
 }
